33d0d44d48c18757e3766b61ac749049ed23e6aa
John Park <jhnprk12@gmail.com>
2019-07-08 12:38:36 -0700

    provider added for connect function

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,5 +1,18 @@
+// import React from 'react';
+// import { render } from 'react-dom';
+// import App from './app';
+
+// render(<App />, document.getElementById('root'));
+
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './app';
+import store from './rootStore';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
