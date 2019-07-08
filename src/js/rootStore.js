92a9ebc9fca944cd083ed7acca4dd254685cf886
John Park <jhnprk12@gmail.com>
2019-07-08 13:22:24 -0700

    reducer and store

--- src/js/rootStore.js
+++ src/js/rootStore.js
@@ -1,11 +1,21 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
-function tempReducer () {
-  return null;
-}
+// function tempReducer () {
+//   return null;
+// }
+
+// const rootStore = createStore(
+//   tempReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+// export default rootStore;
+
+import { createStore } from 'redux';
+import rootReducer from './rootReducer';
 
 const rootStore = createStore(
-  tempReducer,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
